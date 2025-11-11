08a0c93eddac4040f9d249c2d7d302789ae60f89
tuyen pham <phamvantuyenksa@gmail.com>
2025-11-11 17:21:34 +0700

    feat: update code

--- Day03_GPIO_Button/GPIO__button.c
+++ Day03_GPIO_Button/GPIO__button.c
@@ -7,10 +7,10 @@ void Delay_Ms(uint32_t u32Delay)
 {
 	while(u32Delay)
 	{
-		SysTick->LOAD = 72U*1000U - 1U;
-		SysTick->VAL = 0U;
-		SysTick->CTRL =5U;
-		while( !(SysTick->CTRL&(1U<<16U)))
+		SysTick->LOAD = 72U*1000U - 1U; // 72Mhz(~1s) -> 72000 (~1ms)
+		SysTick->VAL = 0U;    // clear curent sysTick value
+		SysTick->CTRL =5U;    // Enable SysTick and select HCLK = 72MHz
+		while( !(SysTick->CTRL&(1U<<16U))) 
 		{ 
 			/* */
 		}
@@ -27,13 +27,13 @@ void GPIO_Config(void)
 
     RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC | RCC_APB2Periph_GPIOA, ENABLE);
 
-    // PC13 là Output Push-Pull (LED)
+    // PC13 as Output Push-Pull (LED)
     GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
     GPIO_InitStructure.GPIO_Pin = GPIO_Pin_13;
     GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
     GPIO_Init(GPIOC, &GPIO_InitStructure);
 
-    // PA1 là Input Pull-up
+    // PA1 as Input Pull-up
     GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPU;
     GPIO_InitStructure.GPIO_Pin = GPIO_Pin_1;
     GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
@@ -45,16 +45,19 @@ void GPIO_Config(void)
 int main()
 {
     GPIO_Config();
-    GPIO_SetBits(GPIOC, GPIO_Pin_13);
+    GPIO_ResetBits(GPIOC, GPIO_Pin_13); // Led ON
 
     while (1)
     {
+			   // check if button (PA1) is pressed (active low)
         if (0U == GPIO_ReadInputDataBit(GPIOA, GPIO_Pin_1))
         {
-            Delay_Ms(100U);
-
+            Delay_Ms(100U); // Decounce delay (100ms)
+           
+					  // check button dung dang nhan khong
             if (0U == GPIO_ReadInputDataBit(GPIOA, GPIO_Pin_1))
             {
+							 // wait until button is released
                 while (0U == GPIO_ReadInputDataBit(GPIOA, GPIO_Pin_1));
                 u32Count++;
                 if (u32Count % 2 == 0)
--- Day03_GPIO_Button/README.md
+++ Day03_GPIO_Button/README.md
@@ -1,10 +1,48 @@
-Mục tiêu bài học
+## Mục tiêu bài học
 
-Phần cứng sử dụng
+Cấu hình PA1 làm ** nút nhấn ** , biết cách ** chống dội ** khi nhấn nút 
+
+## Phần cứng sử dụng
+**STM32F103
+** Nút nhấn đấu PA1
 
 
-Giải thích ngắn cách code hoạt động
+## Giải thích code hoạt động
+1. **Cấu hình GPIO**
+
+- Cấu hình PC13 là chân nháy led , output push-pull 50MHz
+
+- Cấu hình PA1 là **Input Pull-up** : PA1 được dùng làm ngõ vào và được kéo lên
+mức **logic cao (1)** bên trong vi điều khiển bằng điện trở nội bộ
+-> khi nhấn nút : 1->0 (PA1 nối lên 3.3V qua pull-up), thả tay : 0->1 (PA1 
+nối xuống đất)
+-> Không dùng Floating (thả nổi ) vì không có điện trỞ kéo lên hay kéo xuống 
+PA1 bị nhiễu , dao động ngẫu nhiên giữa 0 và 1 -> vi điều khiển đọc sai trạng thái
+Nên dùng pull-down hoặc pull-up.
+- Cấu hình SysTick cứ 1ms là kích hoạt COUNTFLAG 
+
+2. ** Hoạt động hàm chính**
+ - Khởi tạo : 
+      GPIO_Config();
+      GPIO_ResetBits(GPIOC, GPIO_Pin_13);  // LED sáng (active low)
+
+ - Kiểm tra nút nhấn : if (0U == GPIO_ReadInputDataBit(GPIOA, GPIO_Pin_1))
+→ Nếu PA1 ở mức 0, nghĩa là nút đang được nhấn.
+
+ - Chống dội (Debounce): Delay_Ms(100U);
+  Khi nhấn nút thật, tiếp điểm cơ học bị rung (bounce), tạo ra các 
+  xung 0/1 nhanh trong vài mili-giây.
+  Lệnh Delay_Ms(100) giúp chờ tín hiệu ổn định, tránh đọc sai nhiều lần.
+
+ - check lại xem nút có đúng đang nhấn không : 
+if (0U == GPIO_ReadInputDataBit(GPIOA, GPIO_Pin_1))
+-> → Đảm bảo nút thực sự đang được nhấn, không phải nhiễu.
+
+- Đợi cho đến khi nút được nhả ra : 0-> 1
+ while (0U == GPIO_ReadInputDataBit(GPIOA, GPIO_Pin_1));
+
+- Tăng biến đếm lên , nếu là chẵn thì cho led on , nếu lẻ thì led off.
 
 
-Ảnh chụp Terminal log (sau này thêm)
+## Ảnh chụp
 
