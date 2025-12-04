099baeae70fe000b6adbab5b20eb3987fe60870b
tuyen664 <phamvantuyenksa@gmail.com>
2025-12-04 17:46:10 +0700

    Update interrupt_sysTick.c

--- Day06_Interrupt_SysTick/interrupt_sysTick.c
+++ Day06_Interrupt_SysTick/interrupt_sysTick.c
@@ -5,15 +5,16 @@ volatile uint32_t debounceTime = 0;
 
 void GPIO_Config(void);
 void EXTI_Config(void);
-void SysTick_Config_Custom(void);
+void SysTick_Config(void);
 
-int main(void) {
+int main(void) 
+{
     GPIO_Config();
     EXTI_Config();
     SysTick_Config_Custom();
 
     while (1)
-		{
+	{
         
     }
 }
@@ -28,28 +29,28 @@ void GPIO_Config(void) {
     GPIOC->CRH |=  (0x2 << 20);  // Output push-pull, 2 MHz
 
     // PA1: Input pull-up 
-    GPIOA->CRL &= ~(0xF << 4);   // Xóa cau hình cho PA1
+    GPIOA->CRL &= ~(0xF << 4);   // XÃ³a cau hÃ¬nh cho PA1
     GPIOA->CRL |=  (0x8 << 4);   // Input pull-up/down
-    GPIOA->ODR |=  (1 << 1);     // Kéo lên pull-up
+    GPIOA->ODR |=  (1 << 1);     // KÃ©o lÃªn pull-up
 }
 
 
 void EXTI_Config(void)
 {
-    // Gán EXTI1 PA1
-    AFIO->EXTICR[0] &= ~(0xF << 4);  // Xóa bit map EXTI1
+    // GÃ¡n EXTI1 PA1
+    AFIO->EXTICR[0] &= ~(0xF << 4);  // XÃ³a bit map EXTI1
     AFIO->EXTICR[0] |=  (0x0 << 4);  // Chon port A cho line 1
 
-    // Cho phép ngat EXTI1, kích hoat canh xuong
-    EXTI->IMR  |= (1 << 1);   // Cho phép line 1
-    EXTI->FTSR |= (1 << 1);   // Ngat khi có canh xuong
+    // Cho phÃ©p ngat EXTI1, kÃ­ch hoat canh xuong
+    EXTI->IMR  |= (1 << 1);   // Cho phÃ©p line 1
+    EXTI->FTSR |= (1 << 1);   // Ngat khi cÃ³ canh xuong
 
     // Bat ngat EXTI1 trong NVIC
     NVIC_EnableIRQ(EXTI1_IRQn);
 }
 
 
-void SysTick_Config_Custom(void) 
+void SysTick_Config(void) 
 {
     SysTick->LOAD = 72000 - 1;  // 1ms neu HCLK = 72MHz
     SysTick->VAL  = 0;
@@ -58,10 +59,12 @@ void SysTick_Config_Custom(void)
 }
 
 
-void EXTI1_IRQHandler(void) {
-    if (EXTI->PR & (1 << 1)) {     // Kiem tra co ngat line 1
-        EXTI->PR = (1 << 1);      // Xóa co ngat
-        buttonPressed  = 1;         // Ðanh dau có nhan nut
+void EXTI1_IRQHandler(void) 
+{   // Kiem tra co ngat line 1
+    if (EXTI->PR & (1 << 1)) 
+	{ 
+        EXTI->PR = (1 << 1);      // XÃ³a co ngat
+        buttonPressed  = 1;         // Ãanh dau cÃ³ nhan nut
         debounceTime = 50;         // Chong doi 50ms
         NVIC_DisableIRQ(EXTI1_IRQn); // Tam tat ngat
     }
