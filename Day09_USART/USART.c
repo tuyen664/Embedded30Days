b3664a0d966a74abbd15bc12ae616877ea86a3d7
tuyen664 <phamvantuyenksa@gmail.com>
2025-12-04 19:46:26 +0700

    Update USART.c

--- Day09_USART/USART.c
+++ Day09_USART/USART.c
@@ -10,28 +10,28 @@ int main(void)
     while(1)
     {
         USART1_SendString("HELLO!\r\n");
-        for(int i = 0; i < 1000000; i++);  // Temporary delay
+        for(int i = 0; i < 1000000; i++);  // delay
     }
 }
 
 void USART1_Init(void)
 {
-    RCC->APB2ENR |= (1<<2) | (1<<14);   // Enable clock for GPIOA and USART1
+    RCC->APB2ENR |= (1<<2) | (1<<14);   // Enable GPIOA and USART1
 
     GPIOA->CRH &= ~((0xF<<4) | (0xF<<8));
-    GPIOA->CRH |=  (0x0B<<4);           // PA9 (TX): Alternate Function Push-Pull, 50 MHz
+    GPIOA->CRH |=  (0x0B<<4);           // PA9 (TX): AF Push-Pull, 50 MHz
     GPIOA->CRH |=  (0x04<<8);           // PA10 (RX): Input floating mode
 
   
-    USART1->BRR = 0x1D4C;          // Set baud rate to 9600 @72 MHz
+    USART1->BRR = 0x1D4C;          //  baudrate = 9600 
 	
-	 // Enable USART, Transmitter, Receiver (UE=1, TE=1, RE=1)
+	// Enable USART, Transmitter, Receiver (UE=1, TE=1, RE=1)
     USART1->CR1 = (1<<13) | (1<<3) | (1<<2);  
 }
 
 void USART1_SendChar(char c)
 {
-    while(!(USART1->SR & (1<<7))); // Wait until TXE (Transmit Data Register Empty) = 1
+    while(!(USART1->SR & (1<<7))); // Wait TXE = 1
     USART1->DR = c;               // Send character
 }
 
